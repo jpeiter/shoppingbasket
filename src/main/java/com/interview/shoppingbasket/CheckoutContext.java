86fa93b033980218550624dbe7602138f1456994
jean <jpeiter13@gmail.com>
2023-02-03 15:01:33 +0000

    Tests

--- src/main/java/com/interview/shoppingbasket/CheckoutContext.java
+++ src/main/java/com/interview/shoppingbasket/CheckoutContext.java
@@ -1,8 +1,12 @@
 package com.interview.shoppingbasket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class CheckoutContext {
-    private Basket basket;
+    private final Basket basket;
     private double retailPriceTotal = 0.0;
+    private List<Promotion> promotions = new ArrayList<>();
 
     public void setRetailPriceTotal(double retailPriceTotal) {
         this.retailPriceTotal = retailPriceTotal;
@@ -16,8 +20,15 @@ class CheckoutContext {
         return new PaymentSummary(retailPriceTotal);
     }
 
-
     public Basket getBasket() {
         return basket;
+    }
+
+    public List<Promotion> getPromotions() {
+        return promotions;
+    }
+
+    public void setPromotions(List<Promotion> promotions) {
+        this.promotions = promotions;
     }
 }
--- src/test/java/com/interview/shoppingbasket/BasketTest.java
+++ src/test/java/com/interview/shoppingbasket/BasketTest.java
@@ -50,6 +50,51 @@ public class BasketTest {
 
     @Test
     void consolidateBasketTest() {
-        // Exercise - implement the unit test for consolidate items
+        Basket basket = new Basket();
+
+        basket.add("productCode0", "productName0", 1);
+        basket.add("productCode1", "productName1", 2);
+        basket.add("productCode2", "productName2", 3);
+        basket.add("productCode2", "productName2", 4);
+        basket.add("productCode1", "productName1", 5);
+        basket.add("productCode0", "productName0", 6);
+        basket.add("productCode0", "productName0", 7);
+        basket.add("productCode1", "productName1", 8);
+        basket.add("productCode1", "productName1", 9);
+        basket.add("productCode2", "productName2", 10);
+        basket.add("productCode0", "productName2", 11);
+        basket.add("productCode1", "productName2", 12);
+        basket.add("productCode2", "productName2", 13);
+        basket.add("productCode1", "productName2", 14);
+        basket.add("productCode2", "productName2", 15);
+
+        //  before basket consolidation
+        assertEquals(15, basket.getItems().size());
+
+        basket.consolidateItems();
+        List<BasketItem> consolidatedItems = basket.getItems();
+
+        // after consolidation
+        assertEquals(3, consolidatedItems.size());
+
+        BasketItem consolidated0 = consolidatedItems.get(0);
+        BasketItem consolidated1 = consolidatedItems.get(1);
+        BasketItem consolidated2 = consolidatedItems.get(2);
+
+        // assert consolidated codes
+        assertEquals("productCode0", consolidated0.getProductCode());
+        assertEquals("productCode1", consolidated1.getProductCode());
+        assertEquals("productCode2", consolidated2.getProductCode());
+
+        // assert consolidated quantities
+
+        // productCode0 -> (1 + 6 + 7 + 11)
+        assertEquals(25, consolidated0.getQuantity());
+
+        // productCode1 -> (2 + 5 + 8 +  9 + 12 + 14)
+        assertEquals(50, consolidated1.getQuantity());
+
+        // productCode2 -> (3 + 4 + 10 + 13 + 15)
+        assertEquals(45, consolidated2.getQuantity());
     }
 }
